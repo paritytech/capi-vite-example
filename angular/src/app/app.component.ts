3f682a8b75826d9f7db68aa04fbbdf9681700f04
Ryan Lee <ryan@parity.io>
2023-06-12 10:18:52 -0400

    aaaa

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core"
+import { web3Accounts, web3Enable, web3FromSource } from "@polkadot/extension-dapp"
+import { User } from "common"
 
 @Component({
   selector: "app-root",
@@ -8,5 +10,15 @@ import { Component } from "@angular/core"
 export class AppComponent {
   title = "capi-angular-example"
 
-  ngOnInit() {}
+  async ngOnInit() {
+    await web3Enable("capi-next-example")
+    const web3Source = await web3FromSource("polkadot-js")
+    const accounts = await web3Accounts()
+
+    const users = accounts.map(
+      (account, i) => new User(account.address, account.meta.name ?? `Account ${i}`),
+    )
+
+    console.log("hello world", users)
+  }
 }
