c8e2ebf153d9789d6ceade5bb158581e90d30419
Ryan Lee <ryan@parity.io>
2023-06-11 21:33:57 -0400

    dprint formatting

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -1,29 +1,33 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing"
+import { RouterTestingModule } from "@angular/router/testing"
+import { AppComponent } from "./app.component"
 
-describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+describe("AppComponent", () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+    })
+  )
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  it("should create the app", () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
 
   it(`should have as title 'capi-angular-example'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('capi-angular-example');
-  });
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app.title).toEqual("capi-angular-example")
+  })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('capi-angular-example app is running!');
-  });
-});
+  it("should render title", () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    fixture.detectChanges()
+    const compiled = fixture.nativeElement as HTMLElement
+    expect(compiled.querySelector(".content span")?.textContent).toContain(
+      "capi-angular-example app is running!",
+    )
+  })
+})
--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -1,11 +1,11 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css"
+import { Inter } from "next/font/google"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: 'Capi Next Example',
-  description: 'Example App built with Next and Capi',
+  title: "Capi Next Example",
+  description: "Example App built with Next and Capi",
 }
 
 export default function RootLayout({
