ad8d7384b709362453ef94c01795a8a51bfb8bc9
Ryan Lee <ryan@parity.io>
2023-06-14 23:44:52 -0400

    nuke pnpm workspace

--- vite/src/main.ts
+++ vite/src/main.ts
@@ -1,57 +1,64 @@
-import "./style.css";
-import "./console.js";
+import "./style.css"
+import "./console.js"
 
-import { MultiAddress, westend } from "@capi/westend";
-import {
-  web3Accounts,
-  web3Enable,
-  web3FromSource,
-} from "@polkadot/extension-dapp";
-import { ss58 } from "capi";
-import { pjsSender } from "capi/patterns/compat/pjs_sender";
-import { signature } from "capi/patterns/signature/polkadot";
-import { User } from "common";
+import { MultiAddress, westend } from "@capi/westend"
+import { web3Accounts, web3Enable, web3FromSource } from "@polkadot/extension-dapp"
+import { ss58 } from "capi"
+import { pjsSender } from "capi/patterns/compat/pjs_sender"
+import { signature } from "capi/patterns/signature/polkadot"
 
-const extensions = await web3Enable("capi-vite-example");
+const extensions = await web3Enable("capi-vite-example")
+const sender = pjsSender(westend, (await web3FromSource("polkadot-js")).signer as any)
 
-const users = (await web3Accounts()).map(
-  (account, i) => new User(account.address, account.meta.name ?? `Account ${i}`)
-);
+class User {
+  publicKey
+  address
+  sender
+  constructor(readonly ss58Address: string, readonly name: string) {
+    this.publicKey = ss58.decode(this.ss58Address)[1]
+    this.address = MultiAddress.Id(this.publicKey)
+    this.sender = sender(this.ss58Address)
+  }
+}
 
-const alexaDropdown = document.getElementById("alexa") as HTMLSelectElement;
-const billyDropdown = document.getElementById("billy") as HTMLSelectElement;
-const amountInput = document.getElementById("amount") as HTMLInputElement;
-const submitButton = document.getElementById("submit") as HTMLButtonElement;
+const users = (await web3Accounts()).map((account, i) =>
+  new User(account.address, account.meta.name ?? `Account ${i}`)
+)
 
-populateUserDropdown(alexaDropdown);
-populateUserDropdown(billyDropdown);
+const alexaDropdown = document.getElementById("alexa") as HTMLSelectElement
+const billyDropdown = document.getElementById("billy") as HTMLSelectElement
+const amountInput = document.getElementById("amount") as HTMLInputElement
+const submitButton = document.getElementById("submit") as HTMLButtonElement
+
+populateUserDropdown(alexaDropdown)
+populateUserDropdown(billyDropdown)
 submitButton.addEventListener("click", () => {
   transfer(
     users[+alexaDropdown.value],
     users[+billyDropdown.value],
-    BigInt(amountInput.value)
-  );
-});
+    BigInt(amountInput.value),
+  )
+})
 
 function populateUserDropdown(select: Element) {
   for (const [i, user] of users.entries()) {
-    const option = document.createElement("option");
-    option.setAttribute("value", `${i}`);
-    option.text = user.name;
-    select.appendChild(option);
+    const option = document.createElement("option")
+    option.setAttribute("value", `${i}`)
+    option.text = user.name
+    select.appendChild(option)
   }
-  return select;
+  return select
 }
 
 async function transfer(alexa: User, billy: User, amount: bigint) {
   // Reference Billy's free balance.
   const billyFree = westend.System.Account.value(billy.publicKey)
     .unhandle(undefined)
-    .access("data", "free");
+    .access("data", "free")
 
   // Read the initial free.
-  const initialFree = await billyFree.run();
-  console.log("Billy free initial:", initialFree);
+  const initialFree = await billyFree.run()
+  console.log("Billy free initial:", initialFree)
 
   // Create and submit the transaction.
   await westend.Balances.transferAllowDeath({
@@ -62,9 +69,9 @@ async function transfer(alexa: User, billy: User, amount: bigint) {
     .sent()
     .dbgStatus("Transfer:")
     .finalized()
-    .run();
+    .run()
 
   // Read the final free.
-  const finalFree = await billyFree.run();
-  console.log("Billy free final:", finalFree);
+  const finalFree = await billyFree.run()
+  console.log("Billy free final:", finalFree)
 }
